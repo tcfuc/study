f80a3a9e2b831922b7e9ee05207f1e47580492c3
tcfuc <1141614570@qq.com>
2020-12-22 17:21:07 +0800

    commit

--- javascript/test1.js
+++ javascript/test1.js
@@ -1,5 +1,3 @@
-
-
 function guidDeDup() {
     let i = 0;
     let j = 0;
@@ -23,4 +21,35 @@ function guidDeDup() {
     }
 }
 
+function max(a, b) {
+    return (a > b) ? a : b;
+}
 
+/**
+ * 递归
+ * @param {*} capacity 背包容量
+ * @param {*} size 物品大小
+ * @param {*} value 物品价值
+ * @param {*} n 物品下标
+ */
+function knapsack(capacity, size, value, n) {
+    if (n == 0 || capacity == 0) {
+        return 0;
+    }
+    if (size[n - 1] > capacity) {
+        return knapsack(capacity, size, value, n - 1);
+    } else {
+        return max(value[n - 1] + knapsack(capacity - size[n - 1], size, value, n - 1), knapsack(capacity, size, value, n - 1))
+    }
+}
+
+/**
+ * 动态规划
+ * @param {*} capacity 
+ * @param {*} size 
+ * @param {*} value 
+ * @param {*} n 
+ */
+function dKnapsack(capacity, size, value, n) {
+
+}
